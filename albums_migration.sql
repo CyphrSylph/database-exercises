90fd9860ea83e125e4949180a1c64efca41a826e
kmaalig <106278777+kmaalig@users.noreply.github.com>
2022-08-25 11:33:29 -0500

    created sql to drop albums table

---
+++ albums_migration.sql
@@ -0,0 +1,9 @@
+CREATE TABLE albums (
+                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                        artist VARCHAR(50),
+                        name VARCHAR(50),
+                        release_date INT,
+                        sales FLOAT,
+                        genre VARCHAR(50),
+                        PRIMARY KEY (id)
+);
